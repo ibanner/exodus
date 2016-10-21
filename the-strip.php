9c6dcb081fb1f293e7507b7b0013f62283a818c6
Itay Banner <itay@ibanner.co.il>
2016-10-21 23:39:15 +0300

    Adeed the strip, for future breadcrumbs and search form

---
+++ the-strip.php
@@ -0,0 +1,8 @@
+<div class="the-strip">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 breadcrumbs"><?php esc_html_e( 'Here Be Breadcrumbs!', 'exodus' ); ?></div>
+            <div class="col-sm-4 form"><?php get_search_form() ?></div>
+        </div>
+    </div>
+</div>
