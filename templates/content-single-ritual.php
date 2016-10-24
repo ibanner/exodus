c46702a157dcd4eea2afff6be1d15a5edbf602e8
Itay Banner <itay@ibanner.co.il>
2016-10-25 01:02:52 +0300

    added internal link to author info box

--- templates/content-single-ritual.php
+++ templates/content-single-ritual.php
@@ -1,7 +1,7 @@
 <div id="main" class="article">
 <?php exodus_cpt_label(); ?>
     <h1 class="article-title"><?php the_title(); ?></h1>
-    <p class="article-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>\
+    <p class="article-meta"><?php the_date(); ?> by <a href="#author-info"><?php the_author(); ?></a></p>
 <?php if ( has_post_thumbnail() ) { ?>
     <figure class="featured-image">
         <?php the_post_thumbnail( 'full' ); ?>
@@ -10,11 +10,12 @@
     }
 
     exodus_get_article_fields();
+
 ?>
     <div class="the-content">
         <?php the_content(); ?>
     </div><!-- /.the-content -->
-    <div class="author-info">
+    <div id="author-info" class="author-info">
         <?php exodus_author_info(); ?>
     </div><!-- /.author-info -->
 
