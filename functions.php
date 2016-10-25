0a214e2b9880749fd8c1673987757a20b829fa03
Itay Banner <itay@ibanner.co.il>
2016-10-25 18:15:24 +0300

    Boostrap local backup :-/

--- functions.php
+++ functions.php
@@ -94,9 +94,11 @@ add_action( 'after_setup_theme', 'exodus_content_width', 0 );
 ------------------------------------------------- */
 function exodus_scripts() {
     // Bootstrap
-    wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css' , array(), '3.3.6' );
+    // wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css' , array(), '3.3.5' );
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' , array(), '3.3.7' );
     wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js', array(), '1.11.3', true );
-    wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js', array('jquery'), '3.3.6', true );
+    // wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js', array('jquery'), '3.3.6', true );
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/jquery.min.js', array(), '3.3.7', true );
 
     // Local
     wp_enqueue_style( 'bootstrap-blog', get_template_directory_uri() . '/css/blog.css' );
