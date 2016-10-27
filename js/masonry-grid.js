30fa82bd30ea51e32d3b168e2bde6c614daab271
Itay Banner <itay@ibanner.co.il>
2016-10-27 15:18:40 +0300

    Nav links position fix, attempt to RTL masonry grid

--- js/masonry-grid.js
+++ js/masonry-grid.js
@@ -1,5 +1,19 @@
-jQuery('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true
-});
+var x = document.getElementsByTagName("html")[0].getAttribute("dir");
+console.log(x);
+if ( x == 'rtl') {
+    jQuery('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true,
+        originLeft: false
+
+    });
+    console.log('originLeft: false');
+} else {
+    jQuery('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+    });
+    console.log('originLeft: true');
+}
