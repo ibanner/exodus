84cf6a6aced9e516f8858490263f8a22feb445d5
Itay Banner <itay@ibanner.co.il>
2016-10-27 17:07:45 +0300

    sub category links on parent cats

--- category.php
+++ category.php
@@ -1,9 +1,32 @@
-<?php get_header(); ?>
+<?php
+    get_header();
+    $term = get_queried_object();
+//    $sub_cats = get_term_children( $term->term_id , 'category' );
+    $sub_cats = get_categories( array(
+        'orderby' => 'id',
+        'parent'  => $term->term_id,
+        'hide_empty' => false,
+    ) );
+?>
 
     <div class="row">
         <div id="main" class="col-sm-12 blog-main single-column">
             <div class="category category--<?php echo single_cat_title(); ?>">
                 <h1 class="category-title"><?php single_cat_title(); ?></h1>
+                <?php if ( $sub_cats ) : ?>
+                    <h2 class="screen-reader-text"><?php esc_html_e( "Sub-Categories" , 'exodus' ); ?></h2>
+                    <ul class="sub-cats">
+                        <?php
+                        foreach ( $sub_cats as $category ) {
+                            printf( '<li class="sub-cat"><a href="%1$s"><button class="btn btn-info">%2$s</button></a></li>',
+                                esc_url( get_category_link( $category->term_id ) ),
+                                esc_html( $category->name )
+                            );
+                        }
+                        ?>
+                    </ul>
+
+                <?php endif; ?>
                 <?php
                 if ( have_posts() ) : ?>
                     <div class="grid">
@@ -18,7 +41,7 @@
                     <div class="posts-nav col-sm-12"><?php the_posts_navigation(); ?></div>
                 <?php else :
 
-                    echo wpautop( 'Sorry, no posts were found' );
+                    echo wpautop( esc_html_e( "There's Nothing Here yet" , 'exodus' ) );
                     // get_template_part( 'templates/content', 'none' );
 
                 endif; ?>
