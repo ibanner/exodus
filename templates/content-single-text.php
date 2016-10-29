34c579240518de7ac14343062ec78c69a853e171
Itay Banner <itay@ibanner.co.il>
2016-10-30 01:51:58 +0300

    Reflecting changes in custom field names

--- templates/content-single-text.php
+++ templates/content-single-text.php
@@ -1,7 +1,8 @@
 <?php
-$text_source = get_field( 'text_source' );
+$creator = get_field( 'creator' );
 $background = get_field( 'background' );
 $commentary = get_field( 'commentary' );
+$the_text = get_field( 'the_text' );
 ?>
 
 <div class="article article--text">
@@ -9,9 +10,25 @@ $commentary = get_field( 'commentary' );
     <!--<p class="article-meta"><?php /*/*the_date(); */ /* esc_html_e( 'Posted by', 'exodus'); */?> <a href="#author-info"><?php /*the_author(); */?></a></p>-->
 
     <div class="cpt-meta">
-        <span class="caption"><?php exodus_cpt_label(); ?> | <?php esc_html_e( 'Communities', 'exodus'); ?></span>
-        <ul class="the-communities">
-            <?php exodus_the_communities(); ?>
+        <span class="caption"><?php exodus_cpt_label(); ?> | <?php esc_html_e( 'Categories:', 'exodus'); ?></span>
+        <ul class="the-categories">
+            <?php
+            $categories = get_the_category();
+            if (!empty($categories)) { ?>
+            <ul class="the-categories">
+                <?php
+                $cat_count = count($categories);
+                $i = 1;
+                foreach($categories as $category) {
+                    echo '<li class="category caption"><a href="' . esc_url( get_category_link( $category->term_id ) ) . '">' . esc_html( get_cat_name( $category->term_id ) ) . '</a></li>';
+                    if ($i == 6 && $i < $cat_count) {
+                        echo '<li class="category caption more"> [...]</li>';
+                        break;
+                    }
+                    $i++;
+                }
+                }
+            ?>
         </ul>
     </div><!-- /.cpt-meta -->
 
@@ -20,11 +37,11 @@ $commentary = get_field( 'commentary' );
         <div class="content text inlet col-xs-12 col-md-6">
             <h2><?php esc_html_e( 'The Text', 'exodus'); ?></h2>
 
-            <?php the_content(); ?>
+            <?php echo $the_text; ?>
 
             <div class="text-info">
                 <h4 class="caption"><?php esc_html_e( 'Text Source', 'exodus'); ?></h4>
-                <p><?php echo $text_source; ?></p>
+                <p><?php echo $creator; ?></p>
             </div>
         </div><!-- /.content text inlet -->
 
