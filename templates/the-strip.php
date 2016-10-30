4772b6edfbed6a175beb75518faad4caf9fb8880
Itay Banner <itay@ibanner.co.il>
2016-10-30 11:18:55 +0200

    Breadcrumbs for archives and articles

--- templates/the-strip.php
+++ templates/the-strip.php
@@ -1,7 +1,25 @@
 <div class="the-strip">
     <div class="container">
         <div class="row">
-            <div class="col-sm-8 breadcrumbs"><?php esc_html_e( 'Here Be Breadcrumbs!', 'exodus' ); ?></div>
+            <div class="col-sm-8 breadcrumbs">
+                <?php
+                /*if ( function_exists('yoast_breadcrumb') ) {
+                $breadcrumbs = yoast_breadcrumb('
+                <p id="breadcrumb">','</p>
+                ',false);
+                    var_dump($breadcrumbs);
+                }*/
+                if (is_archive()) {
+                    // $case = 'Archive'; var_dump($case);
+                    echo get_category_parents( $cat, true, ' &raquo; ' );
+                } elseif (is_singular()) {
+                    // $case = 'Singular'; var_dump($case);
+                    $category = get_the_category($id);
+                    // var_dump($category[0]);
+                    $parents = get_category_parents( $category[0], true, __( ' &raquo; ' , 'exodus' ) ); ?>
+                <p class="breadcrumbs"><?php echo $parents;?><span class="you-are-here"> <?php the_title();?></span></p>
+                <?php } ?>
+            </div>
             <div class="col-sm-4 form"><?php get_search_form() ?></div>
         </div>
     </div>
