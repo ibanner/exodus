76f777a5579d68e6a1947144645babb7156f52e6
Itay Banner <itay@ibanner.co.il>
2016-11-01 11:28:52 +0200

    Category styling on the homepage

--- index.php
+++ index.php
@@ -4,20 +4,40 @@
         <div class="search-box">
             <?php get_search_form(); ?>
         </div>
-        <ul class="category-tiles">
+        <div class="category-tiles row">
             <?php
-            $args = array(
-                'orderby'    => 'count',
-                'order'      => 'DESC',
-                'title_li'   => '',
-                'hide_empty'    => false,
+
+            $top_cats = get_categories( array(
+                'orderby' => 'id',
+                'hide_empty' => false,
                 'exclude'       => 1,
                 'parent'        => 0
-            );
-            wp_list_categories($args);
-            // get_categories($args);
+            ) );
+
+            foreach ( $top_cats as $category ) {
+                $sub_cats = get_categories( array (
+                        'orderby' => 'id',
+                        'hide_empty' => false,
+                        'parent'        => $category->term_id
+                    )
+                );
+                /*$cat_background_url = get_field("featured-image", "category_' . $category->term_id . '");
+                $default_image = 'http://kolehad.dev/wp-content/themes/exodus/images/default_cat.jpg';
+                $cat_bg_url = ($cat_background_url ? $cat_background_url : $default_image);
+                var_dump($cat_background_url,$cat_bg_url);*/
+
+                echo '<div class="category-wrapper col-xs-12 col-md-6">';
+                echo '<div class="category panel panel-info">';
+                echo '<div class="panel-heading"><h2><a href="' . esc_url( get_category_link($category) ) . '">' . esc_html( $category->name ) .  '</a></h2></div>';
+                echo '<div class="panel-body"><ul class="sub-cat-list">';
+                foreach ( $sub_cats as $sub_cat ) {
+                    echo '<li class="sub-cat-link"><a href="' . esc_url( get_category_link($sub_cat) ) . '">' . esc_html( $sub_cat->name ) .  '</a></li>';
+                }
+                echo '</ul></div>';
+                echo '</div></div>';
+            }
             ?>
-        </ul>
+        </div><!-- /.category-tiles -->
     </div>
     <div class="row">
 
