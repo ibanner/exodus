e3a105f2354d1e27dd03b70117f6f9bd2cb683f2
Itay Banner <itay@ibanner.co.il>
2016-11-06 11:39:54 +0200

    Added error 404 template

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php
+get_header();
+?>
+
+    <div class="row">
+        <div id="main" class="col-sm-12 blog-main single-column">
+            <div class="404 page <?php echo post_class(); ?>">
+                <h1 class="page-title row"><?php the_title(); ?></h1>
+
+                <div class="jumbotron">
+
+                    <?php
+                        echo '<div class="alert alert-warning text-center" role="alert">';
+                        echo '<p class="lead h1">' . __("Page not found.", 'exodus') . '</p>';
+                        echo __("It's not you, it's us.", 'exodus') . ' <a href="' . site_url() . '" class="alert-link">' . __("Here's a link to the homepage", 'exodus') . '</a>.';
+                        echo '</div>';
+                    ?>
+                </div>
+            </div> <!-- /.page -->
+        </div> <!-- /.main -->
+    </div> <!-- /.row -->
+
+<?php get_footer(); ?>
