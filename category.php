3e57333fc59286548c465a6e945105ef1905a2ff
Itay Banner <itay@ibanner.co.il>
2016-11-20 13:04:57 +0200

    changed isotope filters to the new, collapsed version

--- category.php
+++ category.php
@@ -13,32 +13,16 @@
         <div id="main" class="col-sm-12 blog-main single-column">
             <div class="category category--<?php echo single_cat_title(); ?>">
                 <h1 class="category-title row"><?php single_cat_title(); ?></h1>
-                <?php if ( $sub_cats ) : ?>
-                    <h2 class="screen-reader-text"><?php esc_html_e( "Sub-Categories" , 'exodus' ); ?></h2>
-                    <div class="sub-cats isotope-ui row">
-                        <div class="button-group filter-button-group category col-md-6 col-sm-12" data-filter-group="category">
-                            <button class="button btn btn-info btn-isotope sub-cat active" aria-pressed="true" data-filter="*"><?php esc_html_e( "Show All" , 'exodus' ); ?></button>
-                        <?php
-                        foreach ( $sub_cats as $category ) {
-                            echo '<button class="button btn btn-info btn-isotope sub-cat" aria-pressed="false" data-filter=".category-' . esc_html( $category->slug ) . '">' . esc_html( $category->name ) . '</button>
-                            ';
-                        }
+                <h2 class="screen-reader-text"><?php esc_html_e( "Article Filters" , 'exodus' ); ?></h2>
+                <div class="isotope-ui row">
+                    <?php exodus_post_types_tax_filter_ui(); ?>
+                    <?php exodus_post_format_filter_ui(); ?>
+                </div>
 
-                        ?>
-                        </div>
-                        <div class="button-group filter-button-group type col-md-6 col-sm-12" data-filter-group="type">
-                            <button class="button btn btn-warning btn-isotope sub-cat active" aria-pressed="true" data-filter="*"><?php esc_html_e( "Show All" , 'exodus' ); ?></button>
-                            <button class="button btn btn-warning btn-isotope sub-cat" aria-pressed="false" data-filter=".text"><?php esc_html_e( "Text" , 'exodus' ); ?></button>
-                            <button class="button btn btn-warning btn-isotope sub-cat" aria-pressed="false" data-filter=".ritual"><?php esc_html_e( "Ritual" , 'exodus' ); ?></button>
-                            <button class="button btn btn-warning btn-isotope sub-cat" aria-pressed="false" data-filter=".format-video"><?php esc_html_e( "Video" , 'exodus' ); ?></button>
-                        </div>
-                    </div>
-
-                <?php endif; ?>
                 <?php
 
                 if ( have_posts() ) : ?>
-                    <div class="grid">
+                    <div class="grid row section">
 
                         <?php /* Start the Loop */
 
