0a2d3ec2a5fa07e3ef2a628f105d8621245bd3fb
Itay Banner <itay@ibanner.co.il>
2016-11-21 16:23:35 +0200

    Category pages now display subcats or parent label, contextually

--- category.php
+++ category.php
@@ -1,6 +1,8 @@
 <?php
     get_header();
     $term = get_queried_object();
+    $children = get_term_children($term->cat_ID , 'category');
+    $parent = wp_get_term_taxonomy_parent_id($term->cat_ID , 'category');
     $sub_cats = get_categories( array(
         'orderby' => 'id',
         'parent'  => $term->term_id,
@@ -11,8 +13,34 @@
 
     <div class="row">
         <div id="main" class="col-sm-12 blog-main single-column">
-            <div class="category category--<?php echo $term->slug; ?>">
+            <div class="category">
+                <?php
+
+                if ($parent) {
+                    echo '<p id="parent-cat-label" class="caption"><span>' . get_the_category_by_ID($parent) . '</span></p>';
+                }
+                ?>
                 <h1 class="category-title row"><?php single_cat_title(); ?></h1>
+                <?php
+
+                if ($children) {
+                    echo '<nav id="sub-cats" class="navbar navbar-default">
+                    <div class="container-fluid">
+                    <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sub-cat-list" aria-expanded="false">
+                        <i class="fa fa-angle-double-down" aria-hidden="true"></i>
+                        <span class="sr-only">' . esc_html__( "View Subcategories" , "exodus" ) . '</span>
+                    </button>';
+                    echo '<div class="collapse navbar-collapse" id="sub-cat-list">';
+                        echo '<ul class="nav navbar-nav">';
+                        foreach ($children as $child) {
+                            echo '<li><a href="'. get_category_link($child) . '">' . get_the_category_by_ID($child) . '</a></li>';
+                        }
+                        echo '</ul>';
+                    echo '</div>';
+                echo '</div></div></nav>';
+                }
+                ?>
                 <h2 class="screen-reader-text"><?php esc_html_e( "Article Filters" , 'exodus' ); ?></h2>
                 <div class="isotope-ui row">
                     <?php exodus_post_types_tax_filter_ui(); ?>
