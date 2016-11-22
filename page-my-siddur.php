76d851a17ea9cdee67ae0aa46a776dfd94135cb8
Itay Banner <itay@ibanner.co.il>
2016-11-22 02:18:23 +0200

    wip

--- page-my-siddur.php
+++ page-my-siddur.php
@@ -32,49 +32,42 @@ $alert = exodus_siddur_action_handler();
                             echo ' <a href="/wp-login.php" class="alert-link">' . __("You need to log in", 'exodus') . '</a> ' . __( 'or' , 'exodus') . ' <a href="/wp-login.php?action=register" class="alert-link">' . __("sign up", 'exodus') . '</a>' . __( ' to see this page.' , 'exodus');
                             echo '</div></div>';
 
-                        } elseif ( empty($query) ) {
+                        } elseif ( !$query->have_posts() ) {
 
                             echo '<div class="jumbotron">';
                             echo '<div class="alert alert-warning text-center" role="alert">';
                             echo '<p class="lead h1">' . __("Your Siddur is still empty!", 'exodus') . '</p>';
-                            echo ' <a href="/wp-login.php" class="alert-link">' . __("Add your favorite rituals and texts to your Siddur, for quick access.", 'exodus') . '</a>';
+                            echo ' <a href="/wp-login.php" class="alert-link">' . __("Add your favorite items to your Siddur, for quick access.", 'exodus') . '</a>';
                             echo '</div></div>';
 
-                        } elseif ( $query ) { ?>
-
-                            <!--<div class="page-content">
-                                <?php /*the_content(); */?>
-                            </div>-->
+                        } elseif ( $query->have_posts() ) { ?>
 
                             <?php
+                            var_dump( $query->have_posts() );
 
-                            if ( $query->have_posts() ) :
+                            /* Set empty var to collect all post categories */
+                            $categories = '';
 
-                                /* Set empty var to collect all post categories */
-                                $categories = '';
+                            /* Start first Loop */
+                            while ( $query->have_posts() ) : $query->the_post();
 
-                                /* Start first Loop */
-                                while ( $query->have_posts() ) : $query->the_post();
+                                /* Get all categories for each post */
+                                $post_cats = wp_get_post_categories(get_the_ID());
 
-                                    /* Get all catogories for each post */
-                                    $post_cats = wp_get_post_categories(get_the_ID());
+                                /* arrays are converted to strings */
+                                if (is_array($post_cats)) {
+                                    $post_cats = implode(', ' , $post_cats);
+                                }
+                                /* add current post categories to $categories */
+                                if ( empty($categories) ) {
+                                    $categories .= $post_cats; // No delimiter needed for the first insertion
+                                } else {
+                                    $categories .= ', ' . $post_cats;
+                                }
 
-                                    /* arrays are converted to strings */
-                                    if (is_array($post_cats)) {
-                                        $post_cats = implode(', ' , $post_cats);
-                                    }
-                                    /* add current post categories to $categories */
-                                    if ( empty($categories) ) {
-                                        $categories .= $post_cats; // No delimiter needed for the first insertion
-                                    } else {
-                                        $categories .= ', ' . $post_cats;
-                                    }
-
-                                endwhile;
-                                /* Convert back to array, and filter duplicates */
-                                $categories_unique = array_unique(explode(', ' , $categories));
-
-                            endif;
+                            endwhile;
+                            /* Convert back to array, and filter duplicates */
+                            $categories_unique = array_unique(explode(', ' , $categories));
 
                             if ( $categories_unique ) : ?>
                                 <h2 class="screen-reader-text"><?php esc_html_e( "Categories" , 'exodus' ); ?></h2>
