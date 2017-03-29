7ce37126caa4944c80747ca1ecb0c48625aa12e0
Itay Banner <itay@ibanner.co.il>
2017-03-29 09:01:19 +0300

    fix generic page layout

--- page.php
+++ page.php
@@ -3,12 +3,10 @@
 <div class="row">
     <div id="main" class="col-sm-12 blog-main single-column">
         <div class="page <?php echo post_class(); ?>">
-            <h1 class="page-title row"><?php the_title(); ?></h1>
 
         <?php
         if ( have_posts() ) : while ( have_posts() ) : the_post();
 
-            echo get_post_type();
             get_template_part( 'templates/content', get_post_type() );
 
         endwhile; endif;
--- templates/content-page.php
+++ templates/content-page.php
@@ -1,9 +1,4 @@
 <div class="article">
-    <h2 class="article-title"><?php the_title(); ?></h2>
-    <p class="article-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
-    <?php if ( has_post_thumbnail() ) {
-        the_post_thumbnail();
-    } ?>
+    <h1 class="article-title"><?php the_title(); ?></h1>
     <?php the_content(); ?>
-
 </div><!-- /.article -->
