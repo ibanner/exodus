dfc6588b0e9c54aea74ca2eb163603497763f61f
Itay Banner <itay@ibanner.co.il>
2017-06-11 23:08:07 +0300

    enqueuing the .min versions of js files

--- functions/exodus-enqueue.php
+++ functions/exodus-enqueue.php
@@ -21,29 +21,30 @@ function exodus_scripts() {
 
     // Local
     wp_enqueue_style( 'exodus', get_template_directory_uri() . '/css/main.css' );
-    wp_enqueue_script( 'exodus-navigation', get_template_directory_uri() . '/js/navigation.js', array( 'jquery' ), '20151215', true );
+    wp_enqueue_script( 'exodus-navigation', get_template_directory_uri() . '/js/min/navigation.min.js', array( 'jquery' ), '20151215', true );
     wp_localize_script( 'exodus-navigation', 'screenReaderText', array(
         'expand'   => '<span class="screen-reader-text">' . __( 'expand child menu', 'exodus' ) . '</span>',
         'collapse' => '<span class="screen-reader-text">' . __( 'collapse child menu', 'exodus' ) . '</span>',
     ) );
-    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-    wp_enqueue_script( 'exodus-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/min/navigation.min.js', array(), '20151215', true );
+    wp_enqueue_script( 'exodus-skip-link-focus-fix', get_template_directory_uri() . '/js/min/skip-link-focus-fix.min.js', array(), '20151215', true );
+    // wp_enqueue_script( 'live-js', get_template_directory_uri() . '/js/min/live.min.js', array(), '20151215', true );
 
 
 
     if (is_home() || is_front_page()) {
         wp_enqueue_style( 'flexslider', get_template_directory_uri() . '/css/flexslider.css' , array(), '2.6.2' );
-        wp_enqueue_script( 'flexslider', get_template_directory_uri() . '/js/jquery.flexslider.js', array( 'jquery' ), '20151215', true );
+        wp_enqueue_script( 'flexslider', get_template_directory_uri() . '/js/min/jquery.flexslider.min.js', array( 'jquery' ), '20151215', true );
     }
 
     if (is_home() || is_front_page() || is_category() || is_archive() || is_search() || is_page('my-siddur')) {
         wp_enqueue_style( 'ajax-load-more', get_template_directory_uri() . '/css/alm.css' );
         wp_enqueue_script( 'masonry');
         wp_enqueue_script( 'imagesloaded');
-        wp_enqueue_script( 'masonry-alm', get_template_directory_uri() . '/js/masonry-alm.js', array( 'jquery' ), '', true );
+        wp_enqueue_script( 'masonry-alm', get_template_directory_uri() . '/js/min/masonry-alm.min.js', array( 'jquery' ), '', true );
     }
 
-    wp_enqueue_script( 'exodus-plugin-hacks', get_template_directory_uri() . '/js/plugin-hacks.js', array( 'jquery' ), '0.1', true );
+    wp_enqueue_script( 'exodus-plugin-hacks', get_template_directory_uri() . '/js/min/plugin-hacks.min.js', array( 'jquery' ), '0.1', true );
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
