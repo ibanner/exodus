67393589efb84b22f521b9ce07c6f0e812d5e079
Itay Banner <itay@ibanner.co.il>
2017-07-01 13:14:30 +0300

    display SVG language icons from the images directory, instead of the media libaray (WP doesn't allow SVG uploads)

--- functions/vendor/wpml.php
+++ functions/vendor/wpml.php
@@ -17,19 +17,20 @@ if ( ! function_exists( 'exodus_wpml_switch' )) :
     function exodus_wpml_switch() {
         $languages = apply_filters( 'wpml_active_languages', NULL, 'orderby=id&order=desc' );
 
+
         if ( !empty( $languages ) ) {
             echo '<div class="wpml-switch">';
             foreach( $languages as $l ) {
+                $flag_icon = file_get_contents( site_url() . '/wp-content/themes/exodus/images/vectors/lang-' . $l['code'] . '.svg');
 
                 if ( $l['active'] ) {
 
-                    echo file_get_contents($l['country_flag_url']);
-                    // echo '<img class="wpml-switch__flag wpml-switch__flag--active" src="' . $l['country_flag_url'] . '" height="32" alt="' . $l['language_code'] . '" width="32" />';
+                    echo $flag_icon;
 
                 } else {
 
                     echo '<a href="' . $l['url'] . '" class="wpml-switch__link a--block">';
-                    echo file_get_contents($l['country_flag_url']);
+                    echo $flag_icon;
                     echo '</a>';
 
                 }
