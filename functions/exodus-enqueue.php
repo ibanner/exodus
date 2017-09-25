856837427c6d780e16d1592993f08f5c5a7fda18
Itay Banner <itay@ibanner.co.il>
2017-09-25 11:56:53 +0300

    bump to 2.1.1

--- functions/exodus-enqueue.php
+++ functions/exodus-enqueue.php
@@ -30,7 +30,7 @@ function exodus_scripts() {
     if ( is_rtl() ) {
         wp_enqueue_style( 'exodus-rtl', get_template_directory_uri() . '/css/main_rtl.css'  , array ( 'exodus-default' ), '2.1.0' );
     }
-    wp_enqueue_style( 'exodus-default', get_template_directory_uri() . '/css/main.css'  , array(), '2.1.0' );
+    wp_enqueue_style( 'exodus-default', get_template_directory_uri() . '/css/main.css'  , array(), '2.1.1' );
 
 
 
